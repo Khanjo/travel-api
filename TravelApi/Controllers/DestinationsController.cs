f530fb9cd7e902597bff30a92ff92616f3147051
Joshua Khan <Khanjo@gmail.com>
2023-08-14 16:26:05 -0700

    controllers WIP

---
+++ TravelApi/Controllers/DestinationsController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TravelApi.Models;
+
+namespace TravelApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DestinationsController : ControllerBase
+    {
+        private readonly TravelApiContext _db;
+
+        public DestinationsController(TravelApiContext db)
+        {
+            _db = db;
+        }
+    }
+}
---
+++ TravelApi/Controllers/ReviewsController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TravelApi.Models;
+
+namespace TravelApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewsController : ControllerBase
+    {
+        private readonly TravelApiContext _db;
+
+        public ReviewsController(TravelApiContext db)
+        {
+            _db = db;
+        }
+    }
+}
