4ddc83cd7cc9d1891f1feff134aa3b41342376c5
Joshua Khan <Khanjo@gmail.com>
2023-08-14 14:41:12 -0700

    initialize project

---
+++ TravelApi/Models/TravelApiContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace TravelApi.Models
+{
+    public class TravelApiContext : DbContext
+    {
+        public TravelApiContext(DbContextOptions<TravelApiContext> options) : base(options)
+        {
+        }
+    }
+}
