20cc683034f0397c41c561ddae46dac2ade3ef75
Joshua Khan <Khanjo@gmail.com>
2023-08-14 15:15:12 -0700

    create models and initial migration

--- TravelApi/Models/TravelApiContext.cs
+++ TravelApi/Models/TravelApiContext.cs
@@ -4,6 +4,8 @@ namespace TravelApi.Models
 {
     public class TravelApiContext : DbContext
     {
+        DbSet<Destination> Destinations { get; set; }
+        DbSet<Review> Reviews { get; set; }
         public TravelApiContext(DbContextOptions<TravelApiContext> options) : base(options)
         {
         }
